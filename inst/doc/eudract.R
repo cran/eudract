56cd430b37c6e835706bb375714ce30a0b5c2ea0
Simon Bond <simon.bond@addenbrookes.nhs.uk>
2020-02-13 13:20:02 +0000

    version 0.9.1

--- inst/doc/eudract.R
+++ inst/doc/eudract.R
@@ -1,9 +1,9 @@
-## ----meta----------------------------------------------------------------
+## ----meta---------------------------------------------------------------------
 subjectsExposed <- c("Control"=99,"Experimental"=101)
 #count of deaths not in the Safety data. Could be c(0,0)
 deathsExternal <- c("Control"=3,"Experimental"=5)
 
-## ----input, echo=FALSE, results="asis"-----------------------------------
+## ----input, echo=FALSE, results="asis"----------------------------------------
 library(eudract)
 file <- utils:::.getHelpFile(help("safety"))
 tmp <- tempfile()
@@ -12,7 +12,7 @@ out <- readLines(tmp)
 headfoot <- grep("body", out)
 cat(out[(headfoot[1] + 1):(headfoot[2] - 1)], sep = "\n")
 
-## ----counting------------------------------------------------------------
+## ----counting-----------------------------------------------------------------
 safety_statistics <- safety_summary(safety, 
                                     exposed=subjectsExposed, 
                                     excess_deaths = deathsExternal, 
@@ -20,14 +20,14 @@ safety_statistics <- safety_summary(safety,
                                     )
 safety_statistics
 
-## ----xml-----------------------------------------------------------------
+## ----xml----------------------------------------------------------------------
 simple <- tempfile(fileext = ".xml")
 eudract_upload_file <- tempfile(fileext = ".xml")
 simple_safety_xml(safety_statistics, simple)
 eudract_convert(input=simple,
                 output=eudract_upload_file)
 
-## ----check_schema--------------------------------------------------------
+## ----check_schema-------------------------------------------------------------
 myschema <- xml2::read_xml(system.file("extdata","adverseEvents.xsd", package="eudract"))
 aes <- xml2::read_xml(eudract_upload_file)
 check <- xml2::xml_validate(aes,myschema)
